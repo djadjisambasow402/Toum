d7e04b78b96b41ed34759bc4ee9ead82f43e9006
Djadji Samba Sow <120143228+djadjisambasow402@users.noreply.github.com>
2024-03-22 22:00:45 +0100

    Update gke-jenkins-tools.sh

--- script/gke-jenkins-tools.sh
+++ script/gke-jenkins-tools.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo snap install kubectl --classic
-sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+sudo snap install kubectl --classic -y
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
+
+{ 
+sudo apt install python3-pip -y
+pip install docker
+}
